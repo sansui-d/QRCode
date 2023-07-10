27f4d1b1803c8575a3de19c0135e5875599697d6
dengchuandong <sansui1021@163.com>
2023-07-10 22:31:42 +0800

    chore: 引入antd

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Input from '../components/Input/index'
+import { Button } from 'antd';
 import './App.less';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
     <div className="qr-app">
       <div className='qr-app-title'>背景二维码</div>
       <div className='qr-app-dec'>在线生成带背景的二维码</div>
-      <Input></Input>
+      <Button type="primary">Primary Button</Button>
     </div>
   );
 }
