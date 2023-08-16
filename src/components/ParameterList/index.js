203ffe556fefdbd9eec4d651c399f9a032272429
Dylan <71920152+sansui-d@users.noreply.github.com>
2023-08-16 11:41:29 +0000

    8-16

--- src/components/ParameterList/index.js
+++ src/components/ParameterList/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import Input from '../Input'
 import Select from '../Select'
 import Upload from '../Upload'
@@ -14,7 +14,7 @@ const levelOption = [
     { value: 'H', text: '30%', key: 'level3' }
 ]
 
-const iconOption = [
+const iconTypeOption = [
     { value: 0, text: '无', key: 'icon0' },
     { value: 1, text: '自定义', key: 'icon1' },
     { value: 2, text: '微信-小', key: 'icon2' },
@@ -37,7 +37,9 @@ const posTypeOption = [
 function ParameterList(props) {
     const { onChange } = props
     const [level, setLevel] = useState('H');
-    const [icon, setIcon] = useState(svg);
+    const [icon, setIcon] = useState('');
+    const [iconScale, setIconScale] = useState(33);
+    const [iconType, setIconType] = useState(0);
     const [image, setImage] = useState(img);
     const [type, setType] = useState('rect');
     const [size, setSize] = useState(100);
@@ -48,29 +50,39 @@ function ParameterList(props) {
     const [posColor, setPosColor] = useState('#000000')
     const parameterList = [
         { label: '容错率', value: <Select defaultValue={level} options={levelOption} onChange={setLevel} />, key: 'parameter1' },
-        { label: '图标', value: <Select defaultValue={icon} options={iconOption} onChange={setIcon} />, key: 'parameter2' },
-        { label: '背景图片', value: <Upload />, key: 'parameter3' },
-        { label: '信息点样式', value: <Select defaultValue={type} options={typeOption} onChange={setType} />, key: 'parameter4' },
-        { label: '信息点缩放', value: <Input value={size} type='number' onChange={setSize} />, key: 'parameter5' },
-        { label: '信息点不透明度', value: <Input value={opacity} type='number' onChange={setOpacity} />, key: 'parameter6' },
-        { label: '信息点深色', value: <ColorPicker color={darkColor} onChange={setDarkColor} />, key: 'parameter7' },
-        { label: '信息点浅色', value: <ColorPicker color={lightColor} onChange={setLightColor} />, key: 'parameter8' },
-        { label: '定位点样式', value: <Select defaultValue={posType} options={posTypeOption} onChange={setPosType} />, key: 'parameter9' },
-        { label: '定位点颜色', value: <ColorPicker color={posColor} onChange={setPosColor} />, key: 'parameter10' },
+        { label: '图标', value: <Select defaultValue={iconType} options={iconTypeOption} onChange={setIconType} />, key: 'parameter2' },
+        { label: '图标源', value: <Upload setImage={setIcon} />, key: 'parameter3' },
+        { label: '图标缩放', value: <Input value={iconScale} type='number' onChange={setIconScale} />, key: 'parameter4' },
+        { label: '背景图片', value: <Upload setImage={setImage} />, key: 'parameter5' },
+        { label: '信息点样式', value: <Select defaultValue={type} options={typeOption} onChange={setType} />, key: 'parameter6' },
+        { label: '信息点缩放', value: <Input value={size} type='number' onChange={setSize} />, key: 'parameter7' },
+        { label: '信息点不透明度', value: <Input value={opacity} type='number' onChange={setOpacity} />, key: 'parameter8' },
+        { label: '信息点深色', value: <ColorPicker color={darkColor} onChange={setDarkColor} />, key: 'parameter9' },
+        { label: '信息点浅色', value: <ColorPicker color={lightColor} onChange={setLightColor} />, key: 'parameter10' },
+        { label: '定位点样式', value: <Select defaultValue={posType} options={posTypeOption} onChange={setPosType} />, key: 'parameter11' },
+        { label: '定位点颜色', value: <ColorPicker color={posColor} onChange={setPosColor} />, key: 'parameter12' },
     ]
     useEffect(() => {
         onChange && onChange(
-            { level, image, type, size, opacity, darkColor, lightColor, posType, posColor }
+            { icon, iconScale: (Number(iconScale) / 100).toFixed(0), level, image, type, size, opacity, darkColor, lightColor, posType, posColor }
         )
-    }, [level, image, type, size, opacity, darkColor, lightColor, posType, posColor])
+    }, [icon, iconScale, level, image, type, size, opacity, darkColor, lightColor, posType, posColor])
     return (
         <div className="qr-parameter-list">
-            {parameterList.map((item) => (
-                <div className='qr-parameter-item' key={item.key}>
-                    <div className='qr-parameter-label'>{item.label}</div>
-                    {item.value}
-                </div>
-            ))}
+            {parameterList.map((item) => {
+                if ((item.label === '图标源' && iconType != 1) || (item.label === '图标缩放' && iconType == 0)) {
+                    return (
+                        <div className='qr-parameter-item' key={item.key} style={{ display: 'none' }}>
+                            <div className='qr-parameter-label'>{item.label}</div>
+                            {item.value}
+                        </div>)
+                }
+                return (
+                    <div className='qr-parameter-item' key={item.key}>
+                        <div className='qr-parameter-label'>{item.label}</div>
+                        {item.value}
+                    </div>)
+            })}
         </div>
     );
 }
--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -1,10 +1,24 @@
 import React from 'react';
 import './index.less';
 
-function Upload() {
+function Upload(props) {
+    const { img, setImage } = props
+    const handlePickImg = async () => {
+        const arrFile = await window.showOpenFilePicker({
+            types: [{
+                accept: {
+                    'image/*': ['.png', '.gif', '.jpeg', '.jpg', '.webp']
+                }
+            }],
+            multiple: false
+        });
+        const URL = window.URL || window.webkitURL;
+        var imgURL = URL.createObjectURL(await arrFile[0].getFile());
+        setImage(imgURL)
+    }
     return (
         <div className="qr-upload">
-        123
+            <button onClick={handlePickImg}></button>
         </div>
     );
 }
